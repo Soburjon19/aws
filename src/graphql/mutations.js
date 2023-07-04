33cc8dd5cfc90b684503042b707ddae88021bf13
Daminov Soburjon <95844073+Soburjon19@users.noreply.github.com>
2023-07-04 22:36:56 +0500

    aws

---
+++ src/graphql/mutations.js
@@ -0,0 +1,11 @@
+// import { gql } from 'graphql-tag';
+
+// export const createTodo = gql`
+//   mutation CreateTodo($input: CreateTodoInput!) {
+//     createTodo(input: $input) {
+//       id
+//       name
+//       description
+//     }
+//   }
+// `;
---
+++ src/graphql/queries.js
@@ -0,0 +1,13 @@
+// import { gql } from 'graphql-tag';
+
+// export const listTodos = gql`
+//   query ListTodos {
+//     listTodos {
+//       items {
+//         id
+//         name
+//         description
+//       }
+//     }
+//   }
+// `;
